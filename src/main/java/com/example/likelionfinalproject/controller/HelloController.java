9c162574f0cfb538698ef43d85c6f87274b2153a
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-20 17:59:55 +0900

    feat: Responsebody 어노테이션 사용

--- src/main/java/com/example/likelionfinalproject/controller/HelloController.java
+++ src/main/java/com/example/likelionfinalproject/controller/HelloController.java
@@ -3,18 +3,21 @@ package com.example.likelionfinalproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api/v1")
 public class HelloController {
 
     @GetMapping("/hello")
+    @ResponseBody
     public String helloWorld() {
         return "hello";
     }
 
 
     @GetMapping("/bye")
+    @ResponseBody
     public String byeWorld() {
         return "bye";
     }
