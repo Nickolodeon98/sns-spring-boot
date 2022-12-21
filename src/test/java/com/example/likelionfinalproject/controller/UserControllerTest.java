aaff224b68f367171d493161e5f9512ae008fc25
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-21 13:27:10 +0900

    feat: 회원가입의 성공을 테스트하는 컨트롤러 테스트 추가

---
+++ src/test/java/com/example/likelionfinalproject/controller/UserControllerTest.java
@@ -0,0 +1,66 @@
+package com.example.likelionfinalproject.controller;
+
+import com.example.likelionfinalproject.domain.dto.UserJoinRequest;
+import com.example.likelionfinalproject.domain.dto.UserJoinResponse;
+import com.example.likelionfinalproject.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    @DisplayName("회원가입에 성공한다.")
+    @WithMockUser
+    void join_success() throws Exception {
+        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
+                .userId("sjeon0730")
+                .password("1q2w3e4r")
+                .name("전승환")
+                .build();
+
+        UserJoinResponse userJoinResponse = UserJoinResponse.builder().userId("sjeon0730").message("전승환").build();
+
+        given(userService.registerUser(userJoinRequest)).willReturn(userJoinResponse);
+
+        String url = "/api/v1/users/join";
+
+        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        verify(userService).registerUser(userJoinRequest);
+    }
+}
