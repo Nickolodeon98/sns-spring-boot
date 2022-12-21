d8846e290245f39ddf5c3efbb908fe02be83cbdf
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-21 13:38:10 +0900

    feat: 회원가입을 구현하는 API 정의

--- src/main/java/com/example/likelionfinalproject/controller/UserController.java
+++ src/main/java/com/example/likelionfinalproject/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.example.likelionfinalproject.controller;
 
+import com.example.likelionfinalproject.domain.dto.UserJoinRequest;
+import com.example.likelionfinalproject.domain.dto.UserJoinResponse;
+import com.example.likelionfinalproject.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public Response<UserJoinResponse> joinUser(UserJoinRequest userJoinRequest) {
+        UserJoinResponse userJoinResponse = userService.registerUser(userJoinRequest);
+
+        return Response.success(userJoinResponse);
+    }
+
 
 }
