ca72ffa7f78deea8eb4e4ad82253a337f7995e23
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-21 14:19:08 +0900

    feat: 회원가입 실패 시 API 동작에 대한 테스트 추가

--- src/test/java/com/example/likelionfinalproject/controller/UserControllerTest.java
+++ src/test/java/com/example/likelionfinalproject/controller/UserControllerTest.java
@@ -2,7 +2,9 @@ package com.example.likelionfinalproject.controller;
 
 import com.example.likelionfinalproject.domain.dto.UserJoinRequest;
 import com.example.likelionfinalproject.domain.dto.UserJoinResponse;
+import com.example.likelionfinalproject.exception.UserJoinException;
 import com.example.likelionfinalproject.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -35,36 +37,54 @@ class UserControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    UserJoinResponse userJoinResponse;
+    UserJoinRequest userJoinRequest;
+
+    String joinUrl;
+
     @BeforeEach
     void setUp() {
-
-    }
-
-    @Test
-    @DisplayName("회원가입에 성공한다.")
-    @WithMockUser
-    void join_success() throws Exception {
-        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
+        userJoinRequest = UserJoinRequest.builder()
                 .userId("sjeon0730")
                 .password("1q2w3e4r")
                 .name("전승환")
                 .build();
 
-        UserJoinResponse userJoinResponse = UserJoinResponse.builder()
+        userJoinResponse = UserJoinResponse.builder()
                 .userId("sjeon0730")
                 .message("회원가입에 성공했습니다.")
                 .build();
 
+        joinUrl = "api/v1/users/join";
+    }
+
+    @Test
+    @DisplayName("회원가입에 성공한다.")
+    @WithMockUser
+    void success_join() throws Exception {
         given(userService.registerUser(any())).willReturn(userJoinResponse);
 
-        String url = "/api/v1/users/join";
-
-        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(post(joinUrl).contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsBytes(any()))
                 .with(csrf()))
                 .andExpect(status().isOk())
                 .andDo(print());
 
         verify(userService).registerUser(any());
+    }
+
+    @Test
+    @DisplayName("회원가입에 실패한다.")
+    void fail_join() throws Exception {
+        UserJoinRequest duplicateUser = UserJoinRequest.builder().userId("sjeon0730").name("전승환").password("1q2w3e4r").build();
+        given(userService.registerUser(duplicateUser)).willThrow(new UserJoinException());
+
+        mockMvc.perform(post(joinUrl).contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(duplicateUser))
+                .with(csrf()))
+                .andExpect(status().isConflict())
+                .andDo(print());
+
+        verify(userService).registerUser(duplicateUser);
     }
 }
