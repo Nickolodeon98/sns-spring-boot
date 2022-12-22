5a16f1a0d58644db6ae4305f6a414c7f23178f9f
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-22 09:28:58 +0900

    feat: @EqualsAndHashCode 어노테이션 추가
    - 테스트와 실제 컨트롤러에서 사용되는 DTO 의 주소값이 다른 것을 막기 위해 @EqualsAndHashCode 사용

--- src/main/java/com/example/likelionfinalproject/domain/dto/UserLoginRequest.java
+++ src/main/java/com/example/likelionfinalproject/domain/dto/UserLoginRequest.java
@@ -2,11 +2,13 @@ package com.example.likelionfinalproject.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode
 public class UserLoginRequest {
 
     private String userId;
