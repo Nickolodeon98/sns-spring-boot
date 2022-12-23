a600da581f3c045a708133c520adf2e8c24c0e12
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-23 13:44:26 +0900

    feat: 각 테스트가 하나의 컨트롤러만 테스트하도록 @WebMvcTest 에 명시 및 @WithMockUser 사용해서 가상 인증 진행

--- src/test/java/com/example/likelionfinalproject/controller/PostControllerTest.java
+++ src/test/java/com/example/likelionfinalproject/controller/PostControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(PostController.class)
 class PostControllerTest {
 
     @Autowired
@@ -60,6 +61,7 @@ class PostControllerTest {
 
     @Test
     @DisplayName("포스트 작성에 성공한다")
+    @WithMockUser
     public void post_success() throws Exception {
 
         given(postService.createNewPost(postRequest)).willReturn(postResponse);
