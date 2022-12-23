9ff5affae5be6a2512888d77ff5a864f2b5fc67b
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-23 17:34:42 +0900

    feat: 컨트롤러에서 서비스로 사용자 아이디 정보를 보내면 받은 정보를 토대로 User 객체를 찾도록 구현

--- src/main/java/com/example/likelionfinalproject/service/PostService.java
+++ src/main/java/com/example/likelionfinalproject/service/PostService.java
@@ -18,10 +18,12 @@ public class PostService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
-    public PostResponse createNewPost(PostRequest postRequest) {
+    public PostResponse createNewPost(PostRequest postRequest, String authorId) {
         Post post = postRequest.toEntity();
-        User user = userRepository.findByName(postRequest.getAuthor())
-                .orElseThrow(()->new UserException(ErrorCode.USERNAME_NOT_FOUND, postRequest.getAuthor() + "은 없는 아이디입니다."));
+        User user = userRepository.findByUserId(authorId)
+                .orElseThrow(()->new UserException(ErrorCode.USERNAME_NOT_FOUND, authorId + "은 없는 아이디입니다."));
+        // TODO: 이후에 에러를 어떻게 처리할 지 생각
+        // 필터에서 다 걸러져서 아이디가 오는 것이라 아이디가 없을 수는 없음!!!
 
         post.setAuthor(user);
 
