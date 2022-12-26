006a261a77ca6829a8ba7fbb92b5f0d3d746e815
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-27 08:37:10 +0900

    feat: 포스트 수정 요청 DTO 생성

---
+++ src/main/java/com/example/likelionfinalproject/domain/dto/EditPostRequest.java
@@ -0,0 +1,15 @@
+package com.example.likelionfinalproject.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class EditPostRequest {
+
+    private String title;
+    private String body;
+
+}
