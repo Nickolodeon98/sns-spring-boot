88663a63b5d375e2ac7d508f510faee6898c896d
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-26 22:31:52 +0900

    feat: PostService 의 메서드를 활용해서 포스트를 조회하는 API 구현

--- src/main/java/com/example/likelionfinalproject/controller/PostController.java
+++ src/main/java/com/example/likelionfinalproject/controller/PostController.java
@@ -3,12 +3,14 @@ package com.example.likelionfinalproject.controller;
 import com.example.likelionfinalproject.domain.Response;
 import com.example.likelionfinalproject.domain.dto.PostRequest;
 import com.example.likelionfinalproject.domain.dto.PostResponse;
+import com.example.likelionfinalproject.domain.dto.SelectedPostResponse;
 import com.example.likelionfinalproject.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -26,4 +28,11 @@ public class PostController {
         return Response.success(postResponse);
     }
 
+    /* id, 제목, 내용, 작성자, 작성날짜, 수정날짜 조회 */
+    @GetMapping("/{postsId}")
+    @ResponseBody
+    public Response<SelectedPostResponse> postInfoDetails(@RequestParam Long postsId) {
+        SelectedPostResponse selectedPostResponse = postService.acquireSinglePost(postsId);
+        return Response.success(selectedPostResponse);
+    }
 }
