bb8bc7c14684ded995993c734e01d49641e63d57
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-26 22:32:13 +0900

    feat: GetMapping 어노테이션 import

--- src/main/java/com/example/likelionfinalproject/controller/PostController.java
+++ src/main/java/com/example/likelionfinalproject/controller/PostController.java
@@ -8,10 +8,7 @@ import com.example.likelionfinalproject.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/v1/posts")
