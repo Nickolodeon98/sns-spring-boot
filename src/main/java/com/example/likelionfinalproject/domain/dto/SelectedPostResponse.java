0ff98731a1c8894338b008ae8f1b93a50a68ac5c
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-26 22:32:44 +0900

    feat: 주어진 엔티티를 포스트 조회 응답 DTO 로 변환하는 메서드 추가

--- src/main/java/com/example/likelionfinalproject/domain/dto/SelectedPostResponse.java
+++ src/main/java/com/example/likelionfinalproject/domain/dto/SelectedPostResponse.java
@@ -1,5 +1,6 @@
 package com.example.likelionfinalproject.domain.dto;
 
+import com.example.likelionfinalproject.domain.entity.Post;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,4 +19,15 @@ public class SelectedPostResponse {
     private String userName;
     private LocalDateTime createdAt;
     private LocalDateTime lastModifiedAt;
+
+    public static SelectedPostResponse of(Post post) {
+        return SelectedPostResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .body(post.getBody())
+                .userName(post.getAuthor().getUserName())
+                .createdAt(post.getCreatedAt())
+                .lastModifiedAt(post.getLastModifiedAt())
+                .build();
+    }
 }
