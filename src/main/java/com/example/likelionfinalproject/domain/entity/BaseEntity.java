7c6f8e96d734c3c532738cff3bab1afb3df37016
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-26 17:50:34 +0900

    feat: 엔티티 생성 뿐만 아니라 수정 시간도 기록할 수 있도록 필드 변수 추가

--- src/main/java/com/example/likelionfinalproject/domain/entity/BaseEntity.java
+++ src/main/java/com/example/likelionfinalproject/domain/entity/BaseEntity.java
@@ -1,9 +1,11 @@
 package com.example.likelionfinalproject.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
@@ -21,4 +23,8 @@ public class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(updatable = false)
+    private LocalDateTime lastModifiedAt;
 }
