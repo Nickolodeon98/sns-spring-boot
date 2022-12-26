f96e815516c90b0129b55638369ecdc798320be7
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-27 00:56:06 +0900

    feat: 포스트 서비스 레이어에 대한 테스트 시 필요한 객체들을 각 테스트 전에 생성하도록 변경 및 포스트 등록 성공 테스트 작성

--- src/test/java/com/example/likelionfinalproject/service/PostServiceTest.java
+++ src/test/java/com/example/likelionfinalproject/service/PostServiceTest.java
@@ -1,8 +1,12 @@
 package com.example.likelionfinalproject.service;
 
+import com.example.likelionfinalproject.domain.dto.PostRequest;
+import com.example.likelionfinalproject.domain.dto.PostResponse;
 import com.example.likelionfinalproject.domain.dto.SelectedPostResponse;
 import com.example.likelionfinalproject.domain.entity.Post;
 import com.example.likelionfinalproject.domain.entity.User;
+import com.example.likelionfinalproject.exception.ErrorCode;
+import com.example.likelionfinalproject.exception.UserException;
 import com.example.likelionfinalproject.repository.PostRepository;
 import com.example.likelionfinalproject.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
@@ -15,6 +19,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -25,34 +30,59 @@ class PostServiceTest {
 
     UserRepository userRepository = Mockito.mock(UserRepository.class);
     PostService postService;
+    User postAuthor;
+    Post mockPost;
+    Long postsId;
 
     @BeforeEach
     void setUp() {
         postService = new PostService(postRepository, userRepository);
-    }
 
-    @Test
-    @DisplayName("조회하고자 하는 포스트를 찾아 반환한다.")
-    void fetch_post_info() {
+        postsId = 1L;
 
-        Long postsId = 1L;
-
-        User postAuthor = User.builder()
+        postAuthor = User.builder()
                 .userName("작성자")
                 .build();
 
-        Post foundPost = Post.builder()
+        mockPost = Post.builder()
                 .id(postsId)
                 .author(postAuthor)
                 .title("제목")
                 .body("내용")
                 .build();
+    }
 
-        when(postRepository.findById(postsId)).thenReturn(Optional.of(foundPost));
+    @Test
+    @DisplayName("주어진 정보대로 포스트 등록에 성공한다.")
+    void success_add_post() {
+
+        PostRequest postRequest = PostRequest.builder()
+                .title("제목")
+                .body("내용")
+                .build();
+
+        String mockAuthorId = "작성자";
+
+        when(postRepository.save(any())).thenReturn(mockPost);
+        when(userRepository.findByUserName(mockAuthorId)).thenReturn(Optional.of(postAuthor));
+
+        PostResponse postResponse = postService.createNewPost(postRequest, mockAuthorId);
+
+        Assertions.assertDoesNotThrow(()->new UserException(ErrorCode.USERNAME_NOT_FOUND, mockAuthorId + "은 없는 아이디입니다."));
+        Assertions.assertEquals(mockPost.getId(), postResponse.getPostId());
+
+        verify(postRepository).save(any());
+        verify(userRepository).findByUserName(mockAuthorId);
+    }
+
+    @Test
+    @DisplayName("조회하고자 하는 포스트를 찾아 반환한다.")
+    void fetch_post_info() {
+        when(postRepository.findById(postsId)).thenReturn(Optional.of(mockPost));
 
         SelectedPostResponse response = postService.acquireSinglePost(postsId);
 
-        Assertions.assertEquals(foundPost.getAuthor().getUserName(), response.getUserName());
+        Assertions.assertEquals(mockPost.getAuthor().getUserName(), response.getUserName());
 
         verify(postRepository).findById(postsId);
     }
