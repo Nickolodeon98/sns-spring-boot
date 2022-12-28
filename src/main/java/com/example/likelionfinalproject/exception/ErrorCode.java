37f739bbd373611dbaa5be2af23eed5db5e82ff0
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-29 07:05:13 +0900

    feat: ErrorCode 로부터 메시지를 가져올 수 있도록 함

--- src/main/java/com/example/likelionfinalproject/exception/ErrorCode.java
+++ src/main/java/com/example/likelionfinalproject/exception/ErrorCode.java
@@ -5,16 +5,19 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    DUPLICATE_USERNAME(HttpStatus.CONFLICT),
-    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED),
-    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED),
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND),
-    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR);
+    DUPLICATE_USERNAME(HttpStatus.CONFLICT, "UserName 이 중복됩니다."),
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "Not found"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 잘못되었습니다."),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB 에러");
 
     private final HttpStatus httpStatus;
+    private final String message;
 
-    ErrorCode (HttpStatus httpStatus) {
+    ErrorCode (HttpStatus httpStatus, String message) {
         this.httpStatus = httpStatus;
+        this.message = message;
     }
+
 }
