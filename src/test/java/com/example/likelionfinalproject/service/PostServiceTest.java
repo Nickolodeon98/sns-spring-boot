0b9c700f559d5c0f60ebee07260fc54178f290c6
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-29 16:49:27 +0900

    refactor: 관련 테스트들을 @Nested 로 묶음, 포스트 수정 테스트를 @MethodSource 사용해 간결하게 정리

--- src/test/java/com/example/likelionfinalproject/service/PostServiceTest.java
+++ src/test/java/com/example/likelionfinalproject/service/PostServiceTest.java
@@ -11,10 +11,7 @@ import com.example.likelionfinalproject.fixture.PostFixture;
 import com.example.likelionfinalproject.fixture.UserFixture;
 import com.example.likelionfinalproject.repository.PostRepository;
 import com.example.likelionfinalproject.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -31,128 +28,105 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 
 class PostServiceTest {
-
     PostRepository postRepository = Mockito.mock(PostRepository.class);
-
     UserRepository userRepository = Mockito.mock(UserRepository.class);
     PostService postService;
     User mockUser;
     Post mockPost;
     Integer postId;
+
+    User currentMockUser;
     @BeforeEach
     void setUp() {
         postService = new PostService(postRepository, userRepository);
-
         postId = 1;
-
         mockUser = UserFixture.get();
-
         mockPost = PostFixture.get(postId);
+        currentMockUser = UserFixture.get("다른 작성자");
     }
-
-    @Test
-    @DisplayName("포스트 등록 성공")
-    void success_add_post() {
-        when(userRepository.findByUserName(mockUser.getUserName())).thenReturn(Optional.of(mockPost.getAuthor()));
-        when(postRepository.save(any())).thenReturn(mockPost);
-
-        PostResponse postResponse = postService.createPost(mockPost.toRequest(), mockUser.getUserName());
-
-        Assertions.assertDoesNotThrow(()->new UserException(ErrorCode.USERNAME_NOT_FOUND,
-                mockUser.getUserName() + "은 없는 아이디입니다."));
-
-        Assertions.assertEquals(mockPost.getId(), postResponse.getPostId());
-
-        verify(postRepository).save(any());
-        verify(userRepository).findByUserName(mockUser.getUserName());
-    }
-
-    @Test
-    @DisplayName("포스트 등록 실패")
-    void fail_add_post() {
-        when(userRepository.findByUserName(mockUser.getUserName())).thenReturn(Optional.empty());
-
-        UserException e = Assertions.assertThrows(UserException.class,
-                ()->postService.createPost(mockPost.toRequest(), mockUser.getUserName()));
-
-        Assertions.assertEquals(ErrorCode.USERNAME_NOT_FOUND, e.getErrorCode());
-
-        verify(userRepository).findByUserName(mockUser.getUserName());
-    }
-
-    @Test
-    @DisplayName("포스트 조회 성공")
-    void success_fetch_post_info() {
-        when(postRepository.findById(mockPost.getId())).thenReturn(Optional.of(mockPost));
-
-        SelectedPostResponse response = postService.acquirePost(mockPost.getId());
-
-        Assertions.assertEquals(mockPost.getAuthor().getUserName(), response.getUserName());
-
-        verify(postRepository).findById(mockPost.getId());
-    }
-
-//    @Test
-//    @DisplayName("포스트 수정 실패 - 포스트 없음")
-//    void fail_edit_post_not_found() {
-//        when(postRepository.findById(mockPost.getId())).thenReturn(Optional.empty());
-//
-//        when(userRepository.findByUserName(mockPost.getAuthor().getUserName())).thenReturn(Optional.of(mockPost.getAuthor()));
-//
-//        UserException e = Assertions.assertThrows(UserException.class,
-//                ()->postService.editPost(mockPost.toRequest(), mockPost.getId(), mockUser.getUserName()));
-//
-//        Assertions.assertEquals(ErrorCode.POST_NOT_FOUND, e.getErrorCode());
-//
-//    }
 
     private static Stream<Arguments> provideObjectAndErrorCase() {
         Post mockPost = PostFixture.get();
-        return Stream.of(Arguments.of(ErrorCode.POST_NOT_FOUND, Optional.empty(), Optional.of(mockPost.getAuthor())),
-                Arguments.of( ErrorCode.USERNAME_NOT_FOUND, Optional.of(mockPost), Optional.empty()));
+        User mockUser = UserFixture.get("다른 작성자");
+        return Stream.of(Arguments.of(Named.of("포스트 없음", ErrorCode.POST_NOT_FOUND),
+                        Optional.empty(),
+                        Optional.of(mockPost.getAuthor()),
+                        mockPost.getAuthor().getUserName()),
+                Arguments.of( Named.of("작성자 없음", ErrorCode.USERNAME_NOT_FOUND),
+                        Optional.of(mockPost),
+                        Optional.empty(),
+                        mockPost.getAuthor().getUserName()),
+                Arguments.of( Named.of("작성자 사용자 불일치", ErrorCode.INVALID_PERMISSION),
+                        Optional.of(mockPost),
+                        Optional.of(mockPost.getAuthor()),
+                        mockUser.getUserName()));
     }
+    @Nested
+    @DisplayName("포스트 등록")
+    class PostCreation {
 
-    @ParameterizedTest
-    @DisplayName("포스트 수정 실패")
-    @MethodSource("provideObjectAndErrorCase")
-    void fail_edit_post(ErrorCode code, Optional optionalPost, Optional optionalUser) {
-        when(postRepository.findById(mockPost.getId())).thenReturn(optionalPost);
+        @Test
+        @DisplayName("성공")
+        void success_add_post() {
+            when(userRepository.findByUserName(mockUser.getUserName())).thenReturn(Optional.of(mockPost.getAuthor()));
+            when(postRepository.save(any())).thenReturn(mockPost);
 
-        when(userRepository.findByUserName(mockPost.getAuthor().getUserName())).thenReturn(optionalUser);
+            PostResponse postResponse = postService.createPost(mockPost.toRequest(), mockUser.getUserName());
 
-        UserException e = Assertions.assertThrows(UserException.class,
-                ()->postService.editPost(mockPost.toRequest(), mockPost.getId(), mockUser.getUserName()));
+            Assertions.assertDoesNotThrow(() -> new UserException(ErrorCode.USERNAME_NOT_FOUND,
+                    mockUser.getUserName() + "은 없는 아이디입니다."));
 
-        Assertions.assertEquals(code, e.getErrorCode());
+            Assertions.assertEquals(mockPost.getId(), postResponse.getPostId());
+
+            verify(postRepository).save(any());
+            verify(userRepository).findByUserName(mockUser.getUserName());
+        }
+
+        @Test
+        @DisplayName("실패")
+        void fail_add_post() {
+            when(userRepository.findByUserName(mockUser.getUserName())).thenReturn(Optional.empty());
+
+            UserException e = Assertions.assertThrows(UserException.class,
+                    () -> postService.createPost(mockPost.toRequest(), mockUser.getUserName()));
+
+            Assertions.assertEquals(ErrorCode.USERNAME_NOT_FOUND, e.getErrorCode());
+
+            verify(userRepository).findByUserName(mockUser.getUserName());
+        }
     }
+    @Nested
+    @DisplayName("포스트 조회")
+    class PostAcquisition {
+        @Test
+        @DisplayName("성공")
+        void success_fetch_post_info() {
+            when(postRepository.findById(mockPost.getId())).thenReturn(Optional.of(mockPost));
 
-    @Test
-    @DisplayName("수정하려는 포스트의 작성자와 로그인된 사용자가 달라 수정에 실패한다.")
-    void fail_edit_post_not_allowed_user() {
-        User author = User.builder()
-                .userName("작성자1")
-                .build();
+            SelectedPostResponse response = postService.acquirePost(mockPost.getId());
 
-        User currentUser = User.builder().userName("작성자2")
-                .build();
+            Assertions.assertEquals(mockPost.getAuthor().getUserName(), response.getUserName());
 
-        Post postWithAuthor = Post.builder()
-                .author(author)
-                .title("제목")
-                .body("내용")
-                .build();
-
-        when(postRepository.findById(mockPost.getId())).thenReturn(Optional.of(postWithAuthor));
-
-        when(userRepository.findByUserName(postWithAuthor.getAuthor().getUserName())).thenReturn(Optional.of(author));
-
-        UserException e = Assertions.assertThrows(UserException.class,
-                ()->postService.editPost(any(), mockPost.getId(), currentUser.getUserName()));
-
-        Assertions.assertEquals(ErrorCode.INVALID_PERMISSION, e.getErrorCode());
+            verify(postRepository).findById(mockPost.getId());
+        }
     }
+    @Nested
+    @DisplayName("포스트 수정")
+    class PostEdition {
+        @ParameterizedTest
+        @DisplayName("실패")
+        @MethodSource("com.example.likelionfinalproject.service.PostServiceTest#provideObjectAndErrorCase")
+        void fail_edit_post(ErrorCode code, Optional optionalPost, Optional optionalUser, String userName) {
+            when(postRepository.findById(mockPost.getId())).thenReturn(optionalPost);
 
+            when(userRepository.findByUserName(mockPost.getAuthor().getUserName())).thenReturn(optionalUser);
 
+            UserException e = Assertions.assertThrows(UserException.class,
+                    () -> postService.editPost(mockPost.toRequest(), mockPost.getId(), userName));
+
+            Assertions.assertEquals(code, e.getErrorCode());
+        }
+    }
 
     @Test
     @DisplayName("사용자가 존재하지 않아 포스트 삭제에 실패한다.")
