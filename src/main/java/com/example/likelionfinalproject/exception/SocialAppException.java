0bf9369aedd1acdecdc3977fc09e226dce428e7c
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-02 15:14:35 +0900

    refactor: DB 에러를 담당하는 메서드 구현

--- src/main/java/com/example/likelionfinalproject/exception/SocialAppException.java
+++ src/main/java/com/example/likelionfinalproject/exception/SocialAppException.java
@@ -2,12 +2,16 @@ package com.example.likelionfinalproject.exception;
 
 import com.example.likelionfinalproject.domain.Response;
 import io.jsonwebtoken.ExpiredJwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.SQLException;
+
 @ControllerAdvice
+@Slf4j
 public class SocialAppException {
 
     @ResponseBody
@@ -22,6 +26,15 @@ public class SocialAppException {
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<?> expiredTokenExceptionHandler(ExpiredJwtException exception) {
         ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_PERMISSION);
+        return ResponseEntity.status(errorResponse.getErrorCode().getHttpStatus())
+                .body(Response.fail(errorResponse));
+    }
+
+    @ResponseBody
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<?> sqlExceptionHandler(SQLException e) {
+        log.error("DB 에러:{}", e.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.DATABASE_ERROR);
         return ResponseEntity.status(errorResponse.getErrorCode().getHttpStatus())
                 .body(Response.fail(errorResponse));
     }
