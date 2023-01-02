767e23dbffcb4826898b1d9f1993262d245fc652
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-02 17:07:52 +0900

    refactor: Swagger 에서 basic error controller 를 숨기도록 변경

--- src/main/java/com/example/likelionfinalproject/configuration/SwaggerConfig.java
+++ src/main/java/com/example/likelionfinalproject/configuration/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.example.likelionfinalproject.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
