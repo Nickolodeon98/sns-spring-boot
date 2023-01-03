5b0b88884fe83bdd5ac0687087d6b45ebcd802c2
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-03 11:20:05 +0900

    feat: 댓글을 작성하는 API 구현
    - CommentService 를 DI 받도록 변경

--- src/main/java/com/example/likelionfinalproject/controller/PostController.java
+++ src/main/java/com/example/likelionfinalproject/controller/PostController.java
@@ -1,9 +1,8 @@
 package com.example.likelionfinalproject.controller;
 
 import com.example.likelionfinalproject.domain.Response;
-import com.example.likelionfinalproject.domain.dto.PostRequest;
-import com.example.likelionfinalproject.domain.dto.PostResponse;
-import com.example.likelionfinalproject.domain.dto.SelectedPostResponse;
+import com.example.likelionfinalproject.domain.dto.*;
+import com.example.likelionfinalproject.service.CommentService;
 import com.example.likelionfinalproject.service.PostService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
@@ -26,6 +25,7 @@ import springfox.documentation.annotations.ApiIgnore;
 public class PostController {
 
     private final PostService postService;
+    private final CommentService commentService;
 
     @Operation(summary = "포스트 작성", description = "로그인 한 사용자는 포스트 제목과 내용을 작성하여 등록할 수 있다.")
     @PostMapping
@@ -73,4 +73,12 @@ public class PostController {
         return Response.success(postResponse);
     }
 
+    @ResponseBody
+    @PostMapping("/{postId}/comments")
+    public Response<CommentResponse> addComment(@PathVariable Integer postId, @ApiIgnore Authentication authentication,
+                                                @RequestBody CommentRequest commentRequest) {
+        CommentResponse commentResponse = commentService.uploadComment(commentRequest, authentication.getName(), postId);
+
+        return Response.success(commentResponse);
+    }
 }
