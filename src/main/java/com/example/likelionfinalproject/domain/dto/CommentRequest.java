935342d86454373e08057701cbb7736eaa99346c
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-03 11:07:07 +0900

    feat: 댓글 작성 요청 DTO 에 Post 와 User 엔티티 객체를 받아 DTO 를 Comment 엔티티로 변환하는 메서드 추가

--- src/main/java/com/example/likelionfinalproject/domain/dto/CommentRequest.java
+++ src/main/java/com/example/likelionfinalproject/domain/dto/CommentRequest.java
@@ -1,5 +1,8 @@
 package com.example.likelionfinalproject.domain.dto;
 
+import com.example.likelionfinalproject.domain.entity.Comment;
+import com.example.likelionfinalproject.domain.entity.Post;
+import com.example.likelionfinalproject.domain.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,4 +13,12 @@ import lombok.Getter;
 public class CommentRequest {
 
     private String comment;
+
+    public Comment toEntity(Post post, User user) {
+        return Comment.builder()
+                .comment(comment)
+                .postId(post)
+                .userId(user)
+                .build();
+    }
 }
