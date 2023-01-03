81e8614d28903cb8ae079ac4e4e49c4de80498d9
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-03 11:08:02 +0900

    feat: 댓글 작성 시 사용자 아이디와 포스트 고유 아이디를 서비스 단에서 받아서 등록되는 댓글 정보에 포함하도록 구현

--- src/main/java/com/example/likelionfinalproject/service/CommentService.java
+++ src/main/java/com/example/likelionfinalproject/service/CommentService.java
@@ -2,7 +2,14 @@ package com.example.likelionfinalproject.service;
 
 import com.example.likelionfinalproject.domain.dto.CommentRequest;
 import com.example.likelionfinalproject.domain.dto.CommentResponse;
+import com.example.likelionfinalproject.domain.entity.Comment;
+import com.example.likelionfinalproject.domain.entity.Post;
+import com.example.likelionfinalproject.domain.entity.User;
+import com.example.likelionfinalproject.exception.ErrorCode;
+import com.example.likelionfinalproject.exception.UserException;
 import com.example.likelionfinalproject.repository.CommentRepository;
+import com.example.likelionfinalproject.repository.PostRepository;
+import com.example.likelionfinalproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +19,19 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
-    public CommentResponse uploadComment(CommentRequest commentRequest, String userName) {
-        /* TODO: commentRequest 로 부터 comment 내용을 받아서 Comment 엔티티에 저장한다. */
+    private final UserRepository userRepository;
 
-        Comment savedComment = commentRepository.save(commentRequest.toEntity());
+    private final PostRepository postRepository;
+
+    public CommentResponse uploadComment(CommentRequest commentRequest, String userName, Integer postId) {
+        /* TODO: commentRequest 로 부터 comment 내용을 받아서 Comment 엔티티에 저장한다. */
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(()->new UserException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()));
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(()->new UserException(ErrorCode.POST_NOT_FOUND, ErrorCode.POST_NOT_FOUND.getMessage()));
+
+        Comment savedComment = commentRepository.save(commentRequest.toEntity(post, user));
 
         return CommentResponse.of(savedComment);
     }
