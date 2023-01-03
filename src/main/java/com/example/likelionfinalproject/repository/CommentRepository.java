ea8892b5c64c1f090b2f30dd3a19a1a419c6d7fa
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-03 10:49:23 +0900

    feat: Comment 엔티티 관리하는 리포지토리 클래스 생성

--- src/main/java/com/example/likelionfinalproject/repository/CommentRepository.java
+++ src/main/java/com/example/likelionfinalproject/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.example.likelionfinalproject.repository;
 
-public class CommentRepository {
+import com.example.likelionfinalproject.domain.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
 }
