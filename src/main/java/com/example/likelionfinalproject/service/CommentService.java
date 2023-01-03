a41be3cb8cf70ae54940db0fccc9b5c05678d71a
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-03 10:41:21 +0900

    feat: 댓글을 작성하여 등록하는 로직을 수행하는 메서드 구현

--- src/main/java/com/example/likelionfinalproject/service/CommentService.java
+++ src/main/java/com/example/likelionfinalproject/service/CommentService.java
@@ -1,9 +1,13 @@
 package com.example.likelionfinalproject.service;
 
+import com.example.likelionfinalproject.domain.dto.CommentRequest;
+import com.example.likelionfinalproject.domain.dto.CommentResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
 
-
+    public CommentResponse uploadComment(CommentRequest commentRequest, String userName) {
+        /* TODO: commentRequest 로 부터 comment 내용을 받아서 Comment 엔티티에 저장한다. */
+    }
 }
