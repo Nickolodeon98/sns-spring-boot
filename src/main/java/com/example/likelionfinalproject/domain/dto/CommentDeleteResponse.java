f98c061422038d851ee9c157f9c6d7bafd9773c1
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-04 16:23:42 +0900

    feat: 댓글 삭제 응답 DTO 생성

---
+++ src/main/java/com/example/likelionfinalproject/domain/dto/CommentDeleteResponse.java
@@ -0,0 +1,12 @@
+package com.example.likelionfinalproject.domain.dto;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@Setter
+@NoArgsConstructor
+@Builder
+public class CommentDeleteResponse extends ResponseDto {
+    Integer id;
+}
