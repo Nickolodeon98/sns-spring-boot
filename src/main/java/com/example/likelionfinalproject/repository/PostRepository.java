66254ca0e185b9e011ae7eb83a551c44c8d03abe
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-05 10:38:39 +0900

    feat: 조회된 항목들을 페이지로 나눌 수 있도록 조회 메서드에서 Pageable 객체를 매개 변수로 받도록 변경

--- src/main/java/com/example/likelionfinalproject/repository/PostRepository.java
+++ src/main/java/com/example/likelionfinalproject/repository/PostRepository.java
@@ -2,8 +2,9 @@ package com.example.likelionfinalproject.repository;
 
 import com.example.likelionfinalproject.domain.entity.Post;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
-    Page<Post> findAllByAuthorUserName(String userName);
+    Page<Post> findAllByAuthorUserName(String userName, Pageable pageable);
 }
