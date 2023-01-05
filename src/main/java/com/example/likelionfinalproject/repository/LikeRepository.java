94d4cd28453e6c869d646526e38307fc590313b9
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-05 15:37:13 +0900

    feat: Like 엔티티를 관리하는 리포지토리 생성

---
+++ src/main/java/com/example/likelionfinalproject/repository/LikeRepository.java
@@ -0,0 +1,7 @@
+package com.example.likelionfinalproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Integer> {
+
+}
--- src/main/java/com/example/likelionfinalproject/service/LikeService.java
+++ src/main/java/com/example/likelionfinalproject/service/LikeService.java
@@ -3,6 +3,7 @@ package com.example.likelionfinalproject.service;
 import com.example.likelionfinalproject.domain.entity.User;
 import com.example.likelionfinalproject.exception.ErrorCode;
 import com.example.likelionfinalproject.exception.UserException;
+import com.example.likelionfinalproject.repository.LikeRepository;
 import com.example.likelionfinalproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,12 +18,13 @@ public class LikeService {
     public String pushThumbsUp(Integer postId, String userName) {
 
         User user = userRepository.findByUserName(userName)
-                .orElseThrow(()->new UserException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()))
+                .orElseThrow(()->new UserException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()));
 
         Like like = Like.builder()
                 .postId(postId)
                 .userId(user.getId())
                 .build();
+
         likeRepository.save(like);
 
         return "좋아요를 눌렀습니다.";
