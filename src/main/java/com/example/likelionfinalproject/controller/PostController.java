8f77b74757423a9a98a1efb7d453fc13997108aa
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-05 10:19:07 +0900

    feat: 특정 작성자의 사용자 아이디로 작성된 모든 포스트를 조회하는 API 구현

--- src/main/java/com/example/likelionfinalproject/controller/PostController.java
+++ src/main/java/com/example/likelionfinalproject/controller/PostController.java
@@ -116,4 +116,13 @@ public class PostController {
 
         return Response.success(response);
     }
+
+    @Operation(summary = "마이 피드", description = "인증된 사용자는 자신이 작성한 포스트를 모아서 볼 수 있다.")
+    @ResponseBody
+    @GetMapping("/my")
+    public Response<Page<SelectedPostResponse>> retrievePast(@ApiIgnore Authentication authentication) {
+        Page<SelectedPostResponse> recalled = postService.showMyPosts(authentication.getName());
+
+        return Response.success(recalled);
+    }
 }
