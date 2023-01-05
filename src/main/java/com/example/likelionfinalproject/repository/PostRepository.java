19281655767717777b106ea2ef9e4ddbc55477e2
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-05 10:15:00 +0900

    feat: 포스트에서 특정 작성자의 사용자 아이디를 가지고 조회하는 기능 추가

--- src/main/java/com/example/likelionfinalproject/repository/PostRepository.java
+++ src/main/java/com/example/likelionfinalproject/repository/PostRepository.java
@@ -1,7 +1,9 @@
 package com.example.likelionfinalproject.repository;
 
 import com.example.likelionfinalproject.domain.entity.Post;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    Page<Post> findAllByAuthorUserName(String userName);
 }
