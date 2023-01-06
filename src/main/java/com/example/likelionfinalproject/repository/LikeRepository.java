0bf10a4640e76add71519e1207ab5e33c73ed20a
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-06 15:19:07 +0900

    feat: 명시한 포스트에 달린 좋아요 개수를 세는 메서드 추가

--- src/main/java/com/example/likelionfinalproject/repository/LikeRepository.java
+++ src/main/java/com/example/likelionfinalproject/repository/LikeRepository.java
@@ -1,6 +1,7 @@
 package com.example.likelionfinalproject.repository;
 
 import com.example.likelionfinalproject.domain.entity.Like;
+import com.example.likelionfinalproject.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface LikeRepository extends JpaRepository<Like, Integer> {
 
     Optional<Like> findByPostIdAndUserId(Integer postId, Integer id);
+
+    long countByPost(Post post);
 }
