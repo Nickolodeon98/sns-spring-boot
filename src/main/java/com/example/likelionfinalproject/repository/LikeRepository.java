813bb307ac1de017166c7b71b666ae8cf7a8ce06
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-09 17:46:59 +0900

    🔥 Removed annotations that enable query tuning from find method such that disallowed tuning error is prevented

--- src/main/java/com/example/likelionfinalproject/repository/LikeRepository.java
+++ src/main/java/com/example/likelionfinalproject/repository/LikeRepository.java
@@ -12,8 +12,6 @@ public interface LikeRepository extends JpaRepository<LikeEntity, Integer> {
 
     Optional<LikeEntity> findByPostIdAndUserEntityId(Integer postId, Integer id);
 
-    @Modifying
-    @Query
     Optional<LikeEntity> findByPost(Post post);
 
     long countByPost(Post post);
