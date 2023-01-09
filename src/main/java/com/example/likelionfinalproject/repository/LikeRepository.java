27b2469a86328de1696e2988584670c2f33303a5
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-09 15:48:54 +0900

    ✨ Added annotations that enable query tuning such that select query may return wanted rows only

--- src/main/java/com/example/likelionfinalproject/repository/LikeRepository.java
+++ src/main/java/com/example/likelionfinalproject/repository/LikeRepository.java
@@ -3,6 +3,8 @@ package com.example.likelionfinalproject.repository;
 import com.example.likelionfinalproject.domain.entity.LikeEntity;
 import com.example.likelionfinalproject.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -10,6 +12,8 @@ public interface LikeRepository extends JpaRepository<LikeEntity, Integer> {
 
     Optional<LikeEntity> findByPostIdAndUserEntityId(Integer postId, Integer id);
 
+    @Modifying
+    @Query
     Optional<LikeEntity> findByPost(Post post);
 
     long countByPost(Post post);
