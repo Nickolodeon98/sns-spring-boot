67f8d77ae6941c3c4c696f4a963bf0257fd53dad
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-09 15:38:18 +0900

    🚚 Renamed User as UserEntity

--- src/main/java/com/example/likelionfinalproject/repository/LikeRepository.java
+++ src/main/java/com/example/likelionfinalproject/repository/LikeRepository.java
@@ -1,16 +1,16 @@
 package com.example.likelionfinalproject.repository;
 
-import com.example.likelionfinalproject.domain.entity.Like;
+import com.example.likelionfinalproject.domain.entity.LikeEntity;
 import com.example.likelionfinalproject.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface LikeRepository extends JpaRepository<Like, Integer> {
+public interface LikeRepository extends JpaRepository<LikeEntity, Integer> {
 
-    Optional<Like> findByPostIdAndUserId(Integer postId, Integer id);
+    Optional<LikeEntity> findByPostIdAndUserEntityId(Integer postId, Integer id);
 
-    Optional<Like> findByPostId(Integer postId);
+    Optional<LikeEntity> findByPost(Post post);
 
     long countByPost(Post post);
 }
