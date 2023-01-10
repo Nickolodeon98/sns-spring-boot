bdd24b57d834dcd48284753f39b1f1ee738477a2
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-10 09:23:47 +0900

    ✨ Added a new API method that displays every alarm for a user currently logged in

---
+++ src/main/java/com/example/likelionfinalproject/controller/AlarmController.java
@@ -0,0 +1,37 @@
+package com.example.likelionfinalproject.controller;
+
+import com.example.likelionfinalproject.domain.Response;
+import com.example.likelionfinalproject.domain.dto.AlarmResponse;
+import com.example.likelionfinalproject.service.AlarmService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class AlarmController {
+
+    private final AlarmService alarmService;
+
+    @ResponseBody
+    @GetMapping("/alarms")
+    public Response<Page<AlarmResponse>> displayAlarms(@ApiIgnore Authentication authentication,
+                                                       @PageableDefault(size = 20, direction = Sort.Direction.DESC, sort = "lastModifiedAt")
+                                                 Pageable pageable) {
+        Page<AlarmResponse> alarms = alarmService.fetchAllAlarms(pageable, authentication.getName());
+
+        return Response.success(alarms);
+
+    }
+
+}
