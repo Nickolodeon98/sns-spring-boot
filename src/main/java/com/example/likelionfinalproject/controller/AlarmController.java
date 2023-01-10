7e36b24d72cd5f1e3397db67c3336665a7427ea4
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-10 11:10:25 +0900

    📝 Added depiction of alarm API on Swagger document

--- src/main/java/com/example/likelionfinalproject/controller/AlarmController.java
+++ src/main/java/com/example/likelionfinalproject/controller/AlarmController.java
@@ -3,6 +3,8 @@ package com.example.likelionfinalproject.controller;
 import com.example.likelionfinalproject.domain.Response;
 import com.example.likelionfinalproject.domain.dto.response.AlarmResponse;
 import com.example.likelionfinalproject.service.AlarmService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,10 +20,12 @@ import springfox.documentation.annotations.ApiIgnore;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@Api(tags= "ALARM Endpoints")
 public class AlarmController {
 
     private final AlarmService alarmService;
 
+    @Operation(summary = "알람 목록 조회", description= "현재 로그인된 사용자가 작성한 포스트에 발생했던 좋아요와 댓글을 목록으로 볼 수 있다.")
     @ResponseBody
     @GetMapping("/alarms")
     public Response<Page<AlarmResponse>> displayAlarms(@ApiIgnore Authentication authentication,
