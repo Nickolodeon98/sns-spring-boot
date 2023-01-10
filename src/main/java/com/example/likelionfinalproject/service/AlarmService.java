5d98bcc34c3b2a03f2067229c48e4fe1d9482149
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-10 09:32:15 +0900

    ✨ Modified method to classify alarms by userName such that only alarms consistent for a specific user is displayed

--- src/main/java/com/example/likelionfinalproject/service/AlarmService.java
+++ src/main/java/com/example/likelionfinalproject/service/AlarmService.java
@@ -1,18 +1,31 @@
 package com.example.likelionfinalproject.service;
 
 import com.example.likelionfinalproject.domain.dto.AlarmResponse;
+import com.example.likelionfinalproject.domain.entity.Alarm;
+import com.example.likelionfinalproject.domain.entity.UserEntity;
+import com.example.likelionfinalproject.exception.ErrorCode;
+import com.example.likelionfinalproject.exception.UserException;
+import com.example.likelionfinalproject.repository.AlarmRepository;
+import com.example.likelionfinalproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AlarmService {
 
     private final AlarmRepository alarmRepository;
 
+    private final UserRepository userRepository;
+
 
     public Page<AlarmResponse> fetchAllAlarms(Pageable pageable, String userName) {
-        Page<Alarm> alarms = alarmRepository.findAll(pageable);
+        UserEntity userEntity = userRepository.findByUserName(userName)
+                .orElseThrow(()->new UserException(ErrorCode.USERNAME_NOT_FOUND));
+
+        Page<Alarm> alarms = alarmRepository.findAllByUserEntity(userEntity, pageable);
         return alarms.map(AlarmResponse::of);
     }
 }
