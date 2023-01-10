f7b006d4f290240a45f79fe153411b89fe1099ed
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-10 09:15:02 +0900

    ✨ Added a new method that converts given Alarm object into AlarmResponse object

--- src/main/java/com/example/likelionfinalproject/domain/dto/AlarmResponse.java
+++ src/main/java/com/example/likelionfinalproject/domain/dto/AlarmResponse.java
@@ -1,5 +1,6 @@
 package com.example.likelionfinalproject.domain.dto;
 
+import com.example.likelionfinalproject.domain.entity.Alarm;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,4 +17,15 @@ public class AlarmResponse {
     private Integer targetId;
     private String text;
     private LocalDateTime createdAt;
+
+    public static AlarmResponse of(Alarm alarm) {
+        return AlarmResponse.builder()
+                .id(alarm.getId())
+                .alarmType(alarm.getAlarmType())
+                .fromUserId(alarm.getFromUserId())
+                .targetId(alarm.getTargetId())
+                .text(alarm.getText())
+                .createdAt(alarm.getCreatedAt())
+                .build();
+    }
 }
