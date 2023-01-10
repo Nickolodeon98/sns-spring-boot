295e28ca54d365360fd6ee36e230c16ac897ac66
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-10 09:33:05 +0900

    ✨ Added method to classify alarm entity by userName

--- src/main/java/com/example/likelionfinalproject/repository/AlarmRepository.java
+++ src/main/java/com/example/likelionfinalproject/repository/AlarmRepository.java
@@ -2,8 +2,12 @@ package com.example.likelionfinalproject.repository;
 
 
 import com.example.likelionfinalproject.domain.entity.Alarm;
+import com.example.likelionfinalproject.domain.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Integer> {
 
+    Page<Alarm> findAllByUserEntity(UserEntity user, Pageable pageable);
 }
